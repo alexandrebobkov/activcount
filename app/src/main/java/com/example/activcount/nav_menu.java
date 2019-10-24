33f3f4eb5d72240ffef4dd562d4c89e3288f2c38
Alexandre B <alexandre.bobkov@gmail.com>
2019-10-24 10:35:33 -0700

    Added About menu item.

--- app/src/main/java/com/example/activcount/nav_menu.java
+++ app/src/main/java/com/example/activcount/nav_menu.java
@@ -46,7 +46,7 @@ public class nav_menu extends AppCompatActivity {
         // menu should be considered as top level destinations.
         mAppBarConfiguration = new AppBarConfiguration.Builder(
                 R.id.nav_home, R.id.nav_settings, R.id.nav_preferences, R.id.nav_slideshow,
-                R.id.nav_tools, R.id.nav_share, R.id.nav_send)
+                R.id.nav_tools, R.id.nav_about, R.id.nav_share, R.id.nav_send)
                 .setDrawerLayout(drawer)
                 .build();
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
